1cbf792655e7167ed5e18bb188c9385f1a0dd594
s7thiago <thyagosousasilva@gmail.com>
2020-11-04 14:25:32 -0300

    Updating weather data

---
+++ apis/weather/update_weather.cmd
@@ -0,0 +1,1 @@
+wget https://api.hgbrasil.com/weather?key=ef3ef1a1"&"user_ip=remote -outfile "weather.json"; git status; git add .; git status; echo ======ATUALIZANDO REPOSITÓRIO REMOTO======\n\n; git commit -m "Updating weather data"; git push; echo OK!
