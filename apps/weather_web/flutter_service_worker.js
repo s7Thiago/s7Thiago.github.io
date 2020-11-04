d42200bbd3fa1f9019830dcc61d8c43ab127d9cc
s7thiago <thyagosousasilva@gmail.com>
2020-11-04 14:52:50 -0300

    Uploading weather web app

--- apps/weather_web/flutter_service_worker.js
+++ apps/weather_web/flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ee2495c0cc26ac55cbcefc91e8dc4488",
-"/": "ee2495c0cc26ac55cbcefc91e8dc4488",
+"index.html": "280f6de03c0bead08d2209a42e6feb4d",
+"/": "280f6de03c0bead08d2209a42e6feb4d",
 "main.dart.js": "269ad246d84a031eba7722635b216bc7",
 "manifest.json": "f29afb9bdc3010ed3f37dc1b4a61aaef",
 "version.json": "7652660696ea34248dcfa6c2d80244ba"
