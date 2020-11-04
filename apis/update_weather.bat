7c9904e29ca7119345035ccba257c0398913ecc8
s7thiago <thyagosousasilva@gmail.com>
2020-11-04 14:20:35 -0300

    Updating weather data

--- apis/update_weather.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo initializing api request to hgbrasil.com
-echo .....
-start "updating weather data" wget -N https://api.hgbrasil.com/weather?key=ef3ef1a1"&"user_ip=remote -outfile "weather.json"
---
+++ apis/update_weather.cmd
@@ -0,0 +1,2 @@
+
+wget https://api.hgbrasil.com/weather?key=ef3ef1a1"&"user_ip=remote -outfile "weather.json"; git status; git add .; git status; echo \n\nATUALIZANDO REPOSITÓRIO REMOTO\n\n; git commit -m "Updating weather data"; git push; echo OK!
