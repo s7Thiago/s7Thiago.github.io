f6568a0f2164ded583daffd9240d9475e5e43da5
s7thiago <thyagosousasilva@gmail.com>
2020-11-04 14:28:29 -0300

    Rearranging apis path

--- apis/update_weather.cmd
+++
@@ -1,1 +0,0 @@
-wget https://api.hgbrasil.com/weather?key=ef3ef1a1"&"user_ip=remote -outfile "weather.json"; git status; git add .; git status; echo ======ATUALIZANDO REPOSITÓRIO REMOTO======\n\n; git commit -m "Updating weather data"; git push; echo OK!
