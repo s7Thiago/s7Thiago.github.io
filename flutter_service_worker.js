eee328f4ba3a5f9ea4859f79c4fb879f658a0286
s7thiago <thyagosousasilva@gmail.com>
2020-11-18 13:43:55 -0300

    Updating home

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,20 +3,24 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "a86df32663607082eb0e700e08e4bb44",
+  "assets/AssetManifest.json": "05315d420bc91a1c5f07eff7db3a21b8",
+"assets/assets/documents/resume.png": "2e3753cb4a5d25c9676cd1b5349ef552",
+"assets/assets/documents/resume.svg": "a18f7ae05603e05d04b544c4ba9b036e",
+"assets/assets/documents/resume_ai.svg": "db70eb29f39e29593906c17b0be21e37",
+"assets/assets/documents/resume_linked/resume.svg": "3c0874c89c42a8ec4b901d417203d9b0",
 "assets/assets/images/profile-photo-front-art-thiago.png": "531f9412ec2a20e8675b23140cb07253",
 "assets/assets/images/qr-code-contact-square.png": "d57047fbdbc134e1eb2d095d1d57a010",
 "assets/assets/images/qr-code-contact.png": "6d9a067479723f85214a4080baea743c",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "132a5e63b5e510933ab4845577716106",
-"assets/NOTICES": "9c37c795b3e70f9521dbe5b8391f0a73",
+"assets/NOTICES": "ae01e1ca8db0d51a49229a2beffb1b17",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "5618b061cee80a0fd3b4b38fc875b408",
-"/": "5618b061cee80a0fd3b4b38fc875b408",
-"main.dart.js": "5538c25f3b63efb6c4a54e6abbb2dd97",
+"index.html": "708018e378251381f55eea05914996d6",
+"/": "708018e378251381f55eea05914996d6",
+"main.dart.js": "ec715cada179323bec08deb4a4983797",
 "manifest.json": "013e5445f31f8a254c28167d067ec6d7",
 "version.json": "08d5d326be8649af214046767d9064aa"
 };
